13facb5a6bc16b81cccbdbe4fdd41c9f7925c618
Rajiv_220039 <abhinandansah222@gmail.com>
2024-08-14 14:53:09 +0530

    first lecture

---
+++ UNIT1-JavaScript/0-intro.js
@@ -0,0 +1,17 @@
+// Var operation
+var name = "Avi";
+console.log(name);
+var age=22;
+console.log(age);
+var place = "punjab"
+console.log(place);
+
+// function print(userName, userAge){
+//     return "my inspiration place is "+ userName + "\n" + "what place temp was " + userAge+ '';
+// }
+// console.log(display("avi", 25));
+
+function display(course, courseCode){
+    return "I am sturdying "+ course + "\n" + "the course code is " + courseCode+ '';
+}
+console.log(display("React", "Int252"));
